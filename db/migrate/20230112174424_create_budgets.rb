d411fbe86a813f4c19069adb1b1b339f443ebb9f
samuelmoiwa <samuelmoiwa@gmail.com>
2023-02-21 20:30:17 +0000

    update _create_budgets.rb

--- db/migrate/20230112174424_create_budgets.rb
+++ db/migrate/20230112174424_create_budgets.rb
@@ -3,7 +3,7 @@ class CreateBudgets < ActiveRecord::Migration[7.0]
     create_table :budgets do |t|
       t.string :name
       t.decimal :amount
-      t.references :user, null: false, foreign_key: true
+      t.references :author, null: false, foreign_key: true
       t.timestamps
     end
   end
