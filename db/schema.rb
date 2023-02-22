6cfd5a39a5bb081bee0adaace89a7551fc1b3723
samuelmoiwa <samuelmoiwa@gmail.com>
2023-02-22 11:59:13 +0000

    update schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -17,10 +17,10 @@ ActiveRecord::Schema[7.0].define(version: 2023_01_12_201416) do
   create_table "budgets", force: :cascade do |t|
     t.string "name"
     t.decimal "amount"
-    t.bigint "user_id", null: false
+    t.bigint "author_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["user_id"], name: "index_budgets_on_user_id"
+    t.index ["author_id"], name: "index_budgets_on_author_id"
   end
 
   create_table "group_budgets", force: :cascade do |t|
@@ -35,10 +35,10 @@ ActiveRecord::Schema[7.0].define(version: 2023_01_12_201416) do
   create_table "groups", force: :cascade do |t|
     t.string "name"
     t.string "icon"
-    t.bigint "user_id", null: false
+    t.bigint "author_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["user_id"], name: "index_groups_on_user_id"
+    t.index ["author_id"], name: "index_groups_on_author_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -64,8 +64,8 @@ ActiveRecord::Schema[7.0].define(version: 2023_01_12_201416) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
-  add_foreign_key "budgets", "users"
+  add_foreign_key "budgets", "users", column: "author_id"
   add_foreign_key "group_budgets", "budgets"
   add_foreign_key "group_budgets", "groups"
-  add_foreign_key "groups", "users"
+  add_foreign_key "groups", "users", column: "author_id"
 end
