629d9f9419d2c7da0c60c68597b009b3f5b56395
samuelmoiwa <samuelmoiwa@gmail.com>
2023-02-22 11:58:42 +0000

    update group.rb

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,5 +1,5 @@
 class Group < ApplicationRecord
-  belongs_to :user
+  belongs_to :author, class_name: 'User'
   has_many :group_budgets, dependent: :destroy
   has_many :budgets, through: :group_budgets
 end
