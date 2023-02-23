e631da2c1857cb9b5010d4b98adc0bad3af84127
samuelmoiwa <samuelmoiwa@gmail.com>
2023-02-23 17:13:06 +0000

    update routers.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users, path: '', path_names: { sign_in: 'login', sign_out: 'logout', sign_up: 'register' }
   # # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  
   resources :user do
-    resources :groups 
+    resources :groups
     resources :budgets
   end
 
